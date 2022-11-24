195677f9a54fc3769528c7704f2c58f30963ad1a
Abyravka90 <asepcahya.nugraha75@gmail.com>
2022-11-24 13:26:17 +0700

    recently update

--- es6.js
+++ es6.js
@@ -3,25 +3,29 @@
 //     let name = 'jack'
 // }
 // console.log(name)
+// for (var x=1; x<3; x++)
+// {
+//     console.log(x)
+// }console.log(x)
 
-function varTest(){
-    var x = 1
-    if(true){
-        var x = 2
-        console.log(x)
-    }console.log(x)
-}
+// function varTest(){
+//     var x = 1
+//     if(true){
+//         var x = 2
+//         console.log(x)
+//     }console.log(x)
+// }
 
-function letTest(){
-    let x = 1
-    if(true){
-        let x = 2
-        console.log(x)
-    }console.log(x)
-}
+// function letTest(){
+//     let x = 1
+//     if(true){
+//         let x = 2
+//         console.log(x)
+//     }console.log(x)
+// }
 
-varTest()
-letTest()
+// varTest()
+// letTest()
 
 // constant immutable
 // const a = 10;
@@ -29,13 +33,33 @@ letTest()
 // console.log(a)
 
 // template literal
-let name = 'David'
-// let msg = 'welcome '+name+' !';
-// let msg = `welcome ${name}`
+// let nama = 'David'
+// let msg = 'welcome '+ nama +' !';
+// let msg = `welcome ${nama} !`
 // console.log(msg)
 
-let a = 8
-let b = 9
-let msg = `The sum is ${a+b}`
-console.log(msg)
+// let a = 8
+// let b = 9
+// let msg = `The sum is ${a+b}`
+// console.log(msg)
+
+// looping
+// let arr = [0,1,2]
+// for(let x=0;x<arr.length;x++ ){
+//     console.log(arr[x])
+// }
+
+// let obj = {a:1, b:2, c:3}
+// for(let v in obj){
+//     console.log(v);
+// }
+
+// list = ["x","y","z"];
+// for(let val of list){
+//     console.log(val)
+// }
+
+// for(let ch of "Asep"){
+//     console.log(ch);   
+// }
 
